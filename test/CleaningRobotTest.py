90c07baebe65cbfb9e8a4430d7ded8ce50946eeb
lmabille <mabille.l@icloud.com>
2022-12-13 16:31:21 +0100

    Implement 1st User Story

--- test/CleaningRobotTest.py
+++ test/CleaningRobotTest.py
@@ -8,4 +8,17 @@ class CleaningRobotTest(unittest.TestCase):
     """
     Your tests go here
     """
-    pass
+
+    def setUp(self) -> None:
+        self.rb = CleaningRobot(2, 2)
+
+    def test_initialize_robot(self):
+        self.rb.initialize_robot()
+        coordinates = self.rb.pos_y + self.rb.pos_x + self.rb.facing
+        self.assertEqual(coordinates, '00N')
+
+    def test_robot_status(self):
+        self.rb.initialize_robot()
+        coordinates = self.rb.robot_status()
+        self.assertEqual(coordinates, '00N')
+
